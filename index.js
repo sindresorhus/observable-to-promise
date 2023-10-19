f685d42bace6449b6afe6af4c596059dff6e1b10
Marcius Silva <marciusdasilva51@gmail.com>
2023-10-19 20:34:42 +0100

    feature maximumValues added

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import isObservable from 'is-observable';
 import symbolObservable from 'symbol-observable';
 
-export default async function observableToPromise(value) {
+export default async function observableToPromise(value, maximumValues = 0) {
 	if (!isObservable(value)) {
 		throw new TypeError(`Expected an \`Observable\`, got \`${typeof value}\``);
 	}
 
 	const values = [];
+	var count = 0
 
 	return new Promise((resolve, reject) => {
 		value[symbolObservable.default]().subscribe({
 			next(value) {
-				values.push(value);
+				if(maximumValues > 0 && count < maximumValues){
+					values.push(value)
+					maximumValues += 1;
+				}
+				if(maximumValues === 0){
+					values.push(value);
+				}
 			},
 			error: reject,
 			complete() {
