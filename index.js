d5324507053ef53cfbc443c261b6cce7784d227e
Marcius Silva <marciusdasilva51@gmail.com>
2023-10-28 18:31:35 +0100

    interrupt observable for rxjs

--- index.js
+++ index.js
@@ -13,44 +13,44 @@ export default async function observableToPromise(value, {maximumValues = undefi
 		throw new TypeError(`Expected an \`Observable\`, got \`${typeof value}\``);
 	}
 
-	getObservableLibrary(value);
-
 	if (maximumValues < 0) {
 		throw new Error(`Expected \`maximumValues higher than 0\`, got \`${maximumValues}\``);
 	}
+
+	const takeWhile = await getTakeWhileForLibrary(value);
 
 	const values = [];
 	let count = 1;
 
 	return new Promise((resolve, reject) => {
-		value[symbolObservable.default]().subscribe({
-			next(value) {
-				console.log(count);
-				if (maximumValues > 0 && count <= maximumValues) {
-					values.push(value);
-					count += 1;
-				}
+		if (getObservableLibrary(value) === 'RxJS') {
+			value[symbolObservable.default]()
+				.pipe(takeWhile(() => count <= maximumValues))
+				.subscribe({
+					next(value) {
+						console.log(count);
+						if (maximumValues > 0 && count <= maximumValues) {
+							values.push(value);
+							count += 1;
+						}
 
-				if (maximumValues === undefined) {
-					values.push(value);
-				}
-
-				if (maximumValues === (count - 1)) {
-					// eslint-disable-next-line no-warning-comments
-					// TODO close observable or stop subscription
-				}
-			},
-			error: reject,
-			complete() {
-				resolve(values);
-			},
-		});
+						if (maximumValues === undefined) {
+							values.push(value);
+						}
+					},
+					error: reject,
+					complete() {
+						resolve(values);
+					},
+				});
+		}
 	});
 }
 
 function getObservableLibrary(observable) {
 	if (observable.constructor) {
 		const constructorName = observable.constructor.name;
+		console.log('constructorName', constructorName);
 		switch (constructorName) {
 			case 'Observable': {
 				return 'RxJS';
@@ -75,4 +75,20 @@ function getObservableLibrary(observable) {
 	}
 
 	return 'Unknown';
+}
+
+async function getTakeWhileForLibrary(observable) {
+	const library = getObservableLibrary(observable);
+
+	if (library === 'RxJS') {
+		const {takeWhile} = await import('rxjs');
+		return takeWhile;
+	}
+
+	if (library === 'Most.js') {
+		const {takeWhile} = await import('most/dist/most');
+		return takeWhile;
+	}
+
+	return undefined;
 }
